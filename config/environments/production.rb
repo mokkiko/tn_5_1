c98beab19b247586d79caa17cfeb14bdbd793279
mokkiko <maksim_pro@mail.ru>
2021-07-18 16:09:09 +0300

    add db admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "tn_5_1_production"
+  # config.active_job.queue_name_prefix = "tn51_production"
 
   config.action_mailer.perform_caching = false
 
